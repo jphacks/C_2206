fb77c093f4da481a3927f2d5b105a2eca4c320d7
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-10-17 23:17:15 +0900

    #9 update

--- src/components/PopUps.vue
+++ src/components/PopUps.vue
@@ -2,9 +2,7 @@
   <v-row class="mt-3" justify="center">
     <v-dialog v-model="dialog" persistent max-width="600px">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn color="primary" dark v-bind="attrs" v-on="on">
-          たねをまく
-        </v-btn>
+        <v-btn color="primary" dark v-bind="attrs" v-on="on"> たねをまく </v-btn>
       </template>
       <v-card>
         <v-card-title>
@@ -19,14 +17,12 @@
               </v-col>
 
               <v-col cols="12" sm="6">
-                <v-select :items="['べんきょう', 'うんどう', 'おてつだい', 'どくしょ']" filled label="かならずえらんでね" dense required
-                  v-model="what"></v-select>
+                <v-select :items="['べんきょう', 'うんどう', 'おてつだい', 'どくしょ']" filled label="かならずえらんでね" dense required v-model="what"></v-select>
               </v-col>
-
 
               <!-- 「いくら」 -->
 
-              <v-col cols="12" sm="6" class=" pl-14">
+              <v-col cols="12" sm="6" class="pl-14">
                 <div v-if="inits">
                   <p class="text-h5">いくら</p>
                 </div>
@@ -34,11 +30,9 @@
 
               <v-col cols="12" sm="6">
                 <div v-if="inits">
-                  <v-select :items="['かい','さつ','じかん','こ']" filled label="かならずえらんでね" dense required v-model="init">
-                  </v-select>
+                  <v-select :items="['かい', 'さつ', 'じかん', 'こ']" filled label="かならずえらんでね" dense required v-model="init"> </v-select>
                 </div>
               </v-col>
-
 
               <!-- 「かい」 -->
               <v-col cols="12" sm="12">
@@ -46,7 +40,6 @@
                   <v-text-field v-model.number="howMuch" filled suffix="かい" label="すうじをいれてね（半角）" dense required />
                 </div>
               </v-col>
-
 
               <!-- 「さつ」 -->
               <v-col cols="12" sm="12">
@@ -69,10 +62,9 @@
                 </div>
               </v-col>
 
-
               <!-- 「カレンダー」 -->
 
-              <v-col cols="12" sm="12" class=" pl-10">
+              <v-col cols="12" sm="12" class="pl-10">
                 <div v-if="tillWhenToWhen">
                   <p class="text-h6">いつから・いつまで</p>
                 </div>
@@ -83,24 +75,19 @@
                     <v-container>
                       <v-row>
                         <v-col cols="12" sm="6">
-                          <v-menu ref="menu1" v-model="menu1" :close-on-content-click="false"
-                            transition="scale-transition" offset-y max-width="290px" min-width="auto">
+                          <v-menu ref="menu1" v-model="menu1" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px" min-width="auto">
                             <template v-slot:activator="{ on, attrs }">
-                              <v-text-field v-model="date" label="いつから" persistent-hint prepend-icon="mdi-calendar"
-                                v-bind="attrs" @blur="date = parseDate(date)" v-on="on"></v-text-field>
+                              <v-text-field v-model="date" label="いつから" persistent-hint prepend-icon="mdi-calendar" v-bind="attrs" v-on="on"></v-text-field>
                             </template>
                             <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
                           </v-menu>
                         </v-col>
                         <v-col cols="12" sm="6">
-                          <v-menu ref="menu2" v-model="menu2" :close-on-content-click="false"
-                            transition="scale-transition" offset-y max-width="290px" min-width="auto">
+                          <v-menu ref="menu2" v-model="menu2" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px" min-width="auto">
                             <template v-slot:activator="{ on, attrs }">
-                              <v-text-field v-model="dateFormatted" label="いつまで" persistent-hint
-                                prepend-icon="mdi-calendar" v-bind="attrs"
-                                @blur="dateFormatted = parseDate(dateFormatted)" v-on="on"></v-text-field>
+                              <v-text-field v-model="date2" label="いつまで" persistent-hint prepend-icon="mdi-calendar" v-bind="attrs" v-on="on"></v-text-field>
                             </template>
-                            <v-date-picker v-model="dateFormatted" no-title @input="menu2 = false"></v-date-picker>
+                            <v-date-picker v-model="date2" no-title @input="menu2 = false"></v-date-picker>
                           </v-menu>
                         </v-col>
                       </v-row>
@@ -110,99 +97,85 @@
               </v-col>
             </v-row>
           </v-container>
-
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Close
-          </v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Save
-          </v-btn>
+          <v-btn color="blue darken-1" text @click="closeDialog"> Close </v-btn>
+          <v-btn color="blue darken-1" text @click="closeDialog"> Save </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-
   </v-row>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import VueTimepicker from 'vue2-timepicker'
-import 'vue2-timepicker/dist/VueTimepicker.css'
+import VueTimepicker from "vue2-timepicker";
+import "vue2-timepicker/dist/VueTimepicker.css";
 export default {
   name: "LoginForm",
-  data: () => (
-    {
-      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-      dateFormatted: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-      menu1: false,
-      menu2: false,
-      howMuch: undefined,
-      init: '',
-      uid: "",
-      what: undefined,
-    }),
+  data: () => ({
+    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10),
+    date2: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10),
+    menu1: false,
+    menu2: false,
+    howMuch: undefined,
+    init: "",
+    uid: "",
+    what: undefined,
+    dialog: false,
+  }),
 
   methods: {
     //初期の表示設定
 
     formatDate(date) {
-      if (!date) return null
+      if (!date) return null;
 
-      const [year, month, day] = date.split('-')
-      return `${month}/${day}/${year}`
-    },
-    parseDate(date) {
-      if (!date) return null
-
-      const [month, day, year] = date.split('/')
-      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+      const [year, month, day] = date.split("-");
+      return `${month}/${day}/${year}`;
     },
     openPopUp() {
-      this.howMuchTime = false,
-      this.howManyBooks = false,
-      this.howMuchTimes = false,
-      this.tillWhenToWhen = false,
-      this.init = false
-    }
+      (this.howMuchTime = false), (this.howManyBooks = false), (this.howMuchTimes = false), (this.tillWhenToWhen = false), (this.init = false);
+    },
+    closeDialog() {
+      this.dialog = false;
+    },
   },
   computed: {
     ...mapState(["user"]),
 
     computedDateFormatted() {
-      return this.formatDate(this.date)
+      return this.formatDate(this.date);
     },
 
     //表示・非表示
     inits() {
-      return this.what != undefined
+      return this.what != undefined;
     },
     howMuchTime() {
-      return this.init === 'じかん'
+      return this.init === "じかん";
     },
     howManyTimes() {
-      return this.init === 'かい'
+      return this.init === "かい";
     },
     howManyBooks() {
-      return this.init === 'さつ'
+      return this.init === "さつ";
     },
     howManyThings() {
-      return this.init === 'こ'
+      return this.init === "こ";
     },
     tillWhenToWhen() {
-      return this.howMuch != undefined
-    }
+      return this.howMuch != undefined;
+    },
   },
   watch: {
     date() {
-      this.dateFormatted = this.formatDate(this.date)
+      this.dateFormatted = this.formatDate(this.date);
     },
   },
   components: {
-    'vue-timepicker': VueTimepicker,
+    "vue-timepicker": VueTimepicker,
   },
 };
-
 </script>
