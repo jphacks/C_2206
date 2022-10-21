10892e6b79ca260b81d0148dce5e7d8099109d49
waka320 <yukiwakamatsu6a@gmail.com>
2022-10-21 21:52:12 +0900

    ４０４作った 後は画像なおすだけ

---
+++ src/views/NotfoundView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="bg">
+        <v-container>
+            <div class="error">
+        <img src="../assets/404.png" />
+      </div>
+            <v-row class=" mt-6" align-content="center" justify="center">
+                <h3 text-align="center">ページがみつかりません。。。</h3>
+            </v-row>
+            <v-row class=" mt-6" align-content="center" justify="center">
+                <h3 text-align="center">ログインからやりなおしてください。</h3>
+            </v-row>
+            <v-row class=" mt-6" align-content="center" justify="center">
+                <h3 text-align="center">ごめんね。</h3>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+<style>
+.bg {
+  width: 100%;
+  height: 100vh;
+  background-image: url("../assets/background.png");
+}
+
+.error {
+  text-align: center;
+  
+}
+.error img {
+  width: 70%;
+  height: auto;
+ 
+}
+</style>
