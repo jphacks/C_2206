ac60ae4b36703645d99db61453570c159fe5dc9a
waka320 <yukiwakamatsu6a@gmail.com>
2022-10-22 01:19:03 +0900

    404 背景赤いけどかんせい

--- src/views/NotfoundView.vue
+++ src/views/NotfoundView.vue
@@ -1,21 +1,22 @@
 <template>
-    <div class="bg">
-        <v-container>
-            <div class="error">
+  <div class="bg">
+    <v-container>
+      <div class="error">
         <img src="../assets/404.png" />
       </div>
-            <v-row class=" mt-6" align-content="center" justify="center">
-                <h3 text-align="center">ページがみつかりません。。。</h3>
-            </v-row>
-            <v-row class=" mt-6" align-content="center" justify="center">
-                <h3 text-align="center">ログインからやりなおしてください。</h3>
-            </v-row>
-            <v-row class=" mt-6" align-content="center" justify="center">
-                <h3 text-align="center">ごめんね。</h3>
-            </v-row>
-        </v-container>
-    </div>
+      <v-row class=" mt-6" align-content="center" justify="center">
+        <h3 text-align="center">ページがみつかりません。。。</h3>
+      </v-row>
+      <v-row class=" mt-6" align-content="center" justify="center">
+        <h3 text-align="center">ログインからやりなおしてください。</h3>
+      </v-row>
+      <v-row class=" mt-6" align-content="center" justify="center">
+        <h3 text-align="center">ごめんね。</h3>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
 <style>
 .bg {
   width: 100%;
@@ -25,11 +26,10 @@
 
 .error {
   text-align: center;
-  
 }
+
 .error img {
-  width: 70%;
+  width: 40%;
   height: auto;
- 
 }
 </style>
